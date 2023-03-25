21dac0e17b689b4bdd2fb11debeee4b7bfa9d9e9
Kallz02 <akshay.pranav.kalathil@gmail.com>
2023-03-25 16:05:29 +0530

    First Production commit

--- index.js
+++ index.js
@@ -1,71 +1,122 @@
-const http=require("http");//http server
-const { connect } = require("http2");
-const websocketServer=require("websocket").server
-const httpServer=http.createServer();
+const http = require("http");
+const app = require("express")();
+const websocketServer = require("websocket").server
+const httpServer = http.createServer();
+httpServer.listen(9190, () => console.log("Listening.. on 9090"))
+//hashmap clients
+const clients = {};
+const games = {};
 
-httpServer.listen(9090,()=>console.log("Listen on 9000"));
-
-//hashmap to store the details
-const clients={};
-
-//to store games
-const games={};
-
-const wsServer=new websocketServer({
-  "httpServer":httpServer
+const wsServer = new websocketServer({
+    "httpServer": httpServer
 })
-wsServer.on("request",request =>{
-  const connection =request.accept(null,request.origin);//It has to accept request
-  connection.on("open",()=> console.log("opened!"))
-  connection.on("closed",()=> console.log("closed!"))
-  connection.on("message",message=>{
-     const result = JSON.parse(message.utf8Data)//NOTE: Assumes they send JSON 
-    //Recieved Message From CLient
-    //user create new game
-    if(result.method==="create"){
-      const clientID=result.clientID
-      //
-      const gameID=guid();
-      
-      games[gameID]={
-        "id":gameID,
-        "balls":20
-      }
+wsServer.on("request", request => {
+    //connect
+    const connection = request.accept(null, request.origin);
+    connection.on("open", () => console.log("opened!"))
+    connection.on("close", () => console.log("closed!"))
+    connection.on("message", message => {
+        const result = JSON.parse(message.utf8Data)
+        //I have received a message from the client
+        //a user want to create a new game
+        if (result.method === "create") {
+            const clientId = result.clientId;
+            const gameId = guid();
+            games[gameId] = {
+                "id": gameId,
+                "balls": 20,
+                "clients": []
+            }
 
-      const payLoad={
-        "method":"create",
-        "game":games[gameID]
-      }
+            const payLoad = {
+                "method": "create",
+                "game" : games[gameId]
+            }
 
-      const con=clients[clientID].connection;
+            const con = clients[clientId].connection;
+            con.send(JSON.stringify(payLoad));
+        }
 
-      con.send(JSON.stringify(payLoad));
+        //a client want to join
+        if (result.method === "join") {
+
+            const clientId = result.clientId;
+            const game.Id = result.gameId;
+            const game = games[gameId];
+            if (game.clients.length >= 3) 
+            {
+                //sorry max players reach
+                return;
+            }
+            const color =  {"0": "Red", "1": "Green", "2": "Blue"}[game.clients.length]
+            game.clients.push({
+                "clientId": clientId,
+                "color": color
+            })
+            //start the game
+            if (game.clients.length === 3) updateGameState();
+
+            const payLoad = {
+                "method": "join",
+                "game": game
+            }
+            //loop through all clients and tell them that people has joined
+            game.clients.forEach(c => {
+                clients[c.clientId].connection.send(JSON.stringify(payLoad))
+            })
+        }
+        //a user plays
+        if (result.method === "play") {
+            const gameId = result.gameId;
+            const ballId = result.ballId;
+            const color = result.color;
+            let state = games[gameId].state;
+            if (!state)
+                state = {}
+            
+            state[ballId] = color;
+            games[gameId].state = state;
+            
+        }
+
+    })
+
+    //generate a new clientId
+    const clientId = guid();
+    clients[clientId] = {
+        "connection":  connection
     }
 
-
-  })
-
-  //generate client ID
-  const clientID=guid();
-  clients[clientID]={
-    "connection":connection
-  }
-
-//Message to send Back
-  const payLoad={
-    "method":"connect",
-    "clientID":clientID
-  }
-  //Send Back client connect
-  connection.send(JSON.stringify(payLoad))
+    const payLoad = {
+        "method": "connect",
+        "clientId": clientId
+    }
+    //send back the client connect
+    connection.send(JSON.stringify(payLoad))
 
 })
 
+//Fuction to BroadCast State
+function updateGameState(){
 
+    //{"gameid", fasdfsf}
+    for (const g of Object.keys(games)) {
+        const game = games[g]
+        const payLoad = {
+            "method": "update",
+            "game": game
+        }
 
+        game.clients.forEach(c=> {
+            clients[c.clientId].connection.send(JSON.stringify(payLoad))
+        })
+    }
 
+    setTimeout(updateGameState, 500);
+}
 
-//Function To Generate Random ID 
+//Random ID generator 
+
 function S4() {
     return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
 }
@@ -73,9 +124,7 @@ function S4() {
 // then to call it, plus stitch in '4' in the third group
 const guid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
 
+//Serving The Static Files On Same Server(Removing in production build :D)
+app.get("/", (req,res)=> res.sendFile(__dirname + "/index2.html"))
 
-//Serve Static files
-
-const app= require("express")();
-app.get("/",(req,res)=>res.sendFile(__dirname+"/index.html"))
-app.listen(9091,()=>console.log("Listening on port 9091"))
+app.listen(9191, ()=>console.log("Listening on http port 9091"))
